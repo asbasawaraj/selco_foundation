3db10ccca1a0370b8f141f59e4d01ffecdf0c33a
Basawaraj <basawaraj@gmail.com>
2018-01-17 11:46:00 +0000

    Initial Commit

---
+++ selco_foundation/selco_foundation/doctype/test1/test1.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, SELCO Foundation and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Test1', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ selco_foundation/selco_foundation/doctype/who/who.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, SELCO Foundation and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Who', {
+	refresh: function(frm) {
+
+	}
+});
